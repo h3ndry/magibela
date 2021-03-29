2a04fe5483b80a5c340b492c459b9c267fdca322
Hendry <h3khoza@gmail.com>
2021-03-29 18:58:08 +0000

    added few ui component

---
+++ src/components/shared/Half_Square.svelte
@@ -0,0 +1,57 @@
+<style>
+ .half-square {
+     position: absolute;
+     top: 0;
+     left: 0;
+ }
+</style>
+
+
+<svg
+   width="25.998896mm"
+   height="102.13882mm"
+   viewBox="0 0 25.998896 102.13882"
+   class="half-square">
+  <defs
+     id="defs2">
+    <clipPath
+       clipPathUnits="userSpaceOnUse"
+       id="clipPath1127-6">
+      <rect
+         style="opacity:1;fill:#c7392b;fill-opacity:1;stroke:none;stroke-width:124.809;stroke-linecap:round;stroke-miterlimit:4;stroke-dasharray:none"
+         id="rect1129-7"
+         width="338.05606"
+         height="373.4552"
+         x="64.648865"
+         y="593.11981"
+         rx="50"
+         transform="rotate(23.770308)" />
+    </clipPath>
+    <clipPath
+       clipPathUnits="userSpaceOnUse"
+       id="clipPath1127-6-3">
+      <rect
+         style="opacity:1;fill:#c7392b;fill-opacity:1;stroke:none;stroke-width:124.809;stroke-linecap:round;stroke-miterlimit:4;stroke-dasharray:none"
+         id="rect1129-7-6"
+         width="338.05606"
+         height="373.4552"
+         x="64.648865"
+         y="593.11981"
+         rx="50"
+         transform="rotate(23.770308)" />
+    </clipPath>
+  </defs>
+  <g
+     id="layer2"
+     transform="translate(-192.5564,-120.60547)">
+    <rect
+       style="display:inline;opacity:1;fill:#c7392b;fill-opacity:1;stroke-width:1.09653"
+       id="rect44-6-5-1"
+       width="1023.4562"
+       height="1451.7836"
+       x="0"
+       y="0"
+       clip-path="url(#clipPath1127-6-3)"
+       transform="matrix(0.22886234,0,0,0.22886234,192.5564,-13.218499)" />
+  </g>
+</svg>
