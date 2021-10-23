3aa57d59553285896af0986f3af16563d45d285c
Hendry <h3ndry@users.noreply.github.com>
2021-10-23 20:12:36 +0000

    switched to sveltkit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,59 @@
-<script>
-    import Tracks from '../components/Tracks.svelte'
-    import Header from '../components/Header.svelte'
-    import AboutOverview from '../components/AboutOverview.svelte'
+<script context="module" lang="ts">
+	export const prerender = true;
 </script>
 
-<style></style>
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
 
 <svelte:head>
-	<title>Home - Magibela Website</title>
+	<title>Home</title>
 </svelte:head>
 
-<Header />
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
 
-<AboutOverview />
+		to your new<br />SvelteKit app
+	</h1>
 
-<Tracks />
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
 
+	<Counter />
+</section>
 
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
